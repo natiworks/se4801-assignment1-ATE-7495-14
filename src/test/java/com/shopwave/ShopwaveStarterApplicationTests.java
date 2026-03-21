f2caa8c9a5edbe785f19088e2509f338d23a7e83
Natnael Nigatu <natnaelnigatu23@gmail.com>
2026-03-21 18:23:39 +0300

    refactor: move test classes to correct package structure (com.shopwave)

--- src/test/java/com/shopwave/ShopwaveStarterApplicationTests.java
+++ src/test/java/com/shopwave/ShopwaveStarterApplicationTests.java
@@ -1,6 +1,6 @@
 // Student Number: ATE/7495/14
 
-package com.shopwave.shopwavestarter;
+package com.shopwave;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

bdffe78c2474e8985825d4bf40bf805d98eee0cc
Natnael Nigatu <natnaelnigatu23@gmail.com>
2026-03-21 16:44:22 +0300

    Refactor: move main application class to com.shopwave package

--- src/main/java/com/shopwave/ShopwaveStarterApplication.java
+++ src/main/java/com/shopwave/ShopwaveStarterApplication.java
@@ -1,5 +1,5 @@
 // Student Number: ATE/7495/14
-package com.shopwave.shopwavestarter;
+package com.shopwave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
